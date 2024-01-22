bd0eebca9ad66ba28939124e25dd1f8566e8f12b
omerfarukdemiral <omer.frk027@gmail.com>
2024-01-22 14:23:32 +0000

    Code Synced by DhiWise

---
+++ routes/admin/index.js
@@ -0,0 +1,23 @@
+/**
+ * index.js
+ * @description :: index route file of admin platform.
+ */
+
+const express =  require('express');
+const router =  express.Router();
+router.use('/admin/auth',require('./auth'));
+router.use(require('./adminRoutes'));
+router.use(require('./userRoutes'));
+router.use(require('./assetRoutes'));
+router.use(require('./assetCategoryRoutes'));
+router.use(require('./orderRoutes'));
+router.use(require('./stateRoutes'));
+router.use(require('./walletRoutes'));
+router.use(require('./walletTransactionRoutes'));
+router.use(require('./roleRoutes'));
+router.use(require('./projectRouteRoutes'));
+router.use(require('./routeRoleRoutes'));
+router.use(require('./userRoleRoutes'));
+router.use(require('./uploadRoutes'));
+
+module.exports = router;
